b8216aa0d94378cd8f2cc548c437bd087f055813
Jonathan Rubinger <MeaninglessJargon@gmail.com>
2013-08-08 12:40:43 -0400

    Added player cube and movement

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour {
+	
+	public Transform player;
+	float zOffset;
+	
+	// Use this for initialization
+	void Start () {
+	
+		zOffset = transform.position.z - player.position.z;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float playerZ = player.position.z;
+		float cameraZ = transform.position.z;
+		
+		
+		transform.Translate (0, 0, playerZ - cameraZ + zOffset);
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovementControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovementControl : MonoBehaviour {
+	
+	public float xSpeed = 1.0f;
+	public float ySpeed = 1.0f;
+	public float zSpeed = 1.0f;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void FixedUpdate () {
+		
+		float xMove = Input.GetAxis ("Horizontal") * xSpeed * Time.deltaTime;
+		float yMove = Input.GetAxis ("Vertical") * ySpeed * Time.deltaTime;
+		float zMove = zSpeed * Time.deltaTime;
+		
+		transform.Translate (xMove, yMove, zMove);
+		//rigidbody.MovePosition(new Vector3(transform.position.x + xMove, transform.position.y + yMove, transform.position.z + zMove));
+	}
+	
+	
+}
